e7d523c6d844d501dd66ba44062cf3a2baddffd6
388 สิธินนท์ <118279609+pongsittinon@users.noreply.github.com>
2024-09-12 09:19:16 +0700

    Add files via upload

---
+++ Testunit-388-main/src/pages/IndexPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ head }}</h1>
+    <form>
+      <input type="text" id="firstName" v-model="firstName" />
+      <input type="text" id="secondName" v-model="secondName" />
+      <input type="text" id="Codename" v-model="Codename" />
+      <input type="text" id="nickname" v-model="nickname" />
+      <button @click="submitForm()" class="submitForm">Submit</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputPage',
+  data() {
+    return {
+      head: 'Vue is awesome.',
+      firstName: 'นายสิธินนท์',
+      secondName: '-',
+      Codename:'6604101388',
+      nickname:'ปิงปอง',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form')
+    },
+  }
+}
+</script>
+
+<style scoped></style>
